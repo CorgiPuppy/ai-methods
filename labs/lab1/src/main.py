03a1cfe2914822eb7c51a20e096b4a4a253e152f
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2026-03-05 16:46:29 +0300

    feat: add showing a chart.

--- labs/lab1/src/main.py
+++ labs/lab1/src/main.py
@@ -1,6 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('data/diamonds.csv')
+df = pd.read_csv("data/diamonds.csv")
 
 print(df.head())
 print(df.info())
+
+plt.figure(figsize=(8, 4))
+sns.histplot(df['price'], bins=50, kde=True)
+plt.title("Распределение цен на бриллианты")
+plt.show()
